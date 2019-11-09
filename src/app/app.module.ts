be8df188070b1a5574c8d19c6c2b4067482816e1
Johnny Sakellion <sakellion.john@gmail.com>
2019-11-09 13:28:04 +0200

    Added some basic compoents

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ArticleListComponent } from './article-list/article-list.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    ArticleListComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
